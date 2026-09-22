9b05ef0c2c25503a0e957b146c6615c2b52b3777
Raainshe <ryanbwgt@gmail.com>
2026-09-22 10:46:30 +0200

    ex2

---
+++ ex2/ft_plot_area.py
@@ -0,0 +1,7 @@
+def ft_plot_area():
+    print("Enter length: " ,end="")
+    iLen = int(input())
+    print("Enter width: ", end="")
+    iwidth = int(input())
+
+    print(f"Plot area: {iwidth * iLen}")
